24abb14514ae6c9ec1440613453baf7ce381a927
Mateus <mateus@harpia.tech>
2024-01-24 17:40:17 -0300

    mudanças na paginação

--- blueprints/home/info_pagamentos/info_pagamentos.py
+++ blueprints/home/info_pagamentos/info_pagamentos.py
@@ -13,32 +13,9 @@ def index():
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login.index'))
       
-    else:     
-        # Número de itens por página
-        itens_por_pagina = 10
-
-        # Obtém o número da página atual a partir da query string, padrão é 1
-        pagina = int(request.args.get('pagina', 1))
-
-        # Obtém todos os registros do banco de dados
+    else:
         pagamentos = Pagamentos.query.all()
-
-        # Calcula o número total de páginas
-        total_paginas = len(pagamentos) // itens_por_pagina + (len(pagamentos) % itens_por_pagina > 0)
-        
-        # Verifica se a página atual está além do total de páginas
-        if pagina > total_paginas:
-            # Redireciona para a última página com dados preenchidos
-            return redirect(url_for('.index', pagina=total_paginas))
-
-        # Calcula o índice inicial e final dos itens a serem exibidos
-        indice_inicial = (pagina - 1) * itens_por_pagina
-        indice_final = indice_inicial + itens_por_pagina
-
-        # Obtém os itens da página atual
-        itens_pagina = pagamentos[indice_inicial:indice_final]
-
-        return render_template('home/info_pagamentos/info_pagamentos.html', pagamentos=itens_pagina, total_paginas=total_paginas, pagina_atual=pagina)
+        return render_template('home/info_pagamentos/info_pagamentos.html', pagamentos=pagamentos)
     
 
 @tratativas.route('/filtro-por-email', methods=['GET','POST'])
